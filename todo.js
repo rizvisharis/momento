27a20a590a9753b800d5b8302270105b50f63cfb
Rizvi <rizvisharis@gmail.com>
2020-11-14 16:47:45 +0530

    delete todo list for each element

--- todo.js
+++ todo.js
@@ -4,7 +4,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDO(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (todo) {
+    return todo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,6 +25,7 @@ function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerHTML = "❌";
+  delBtn.addEventListener("click", deleteToDO);
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   span.innerText = text;
