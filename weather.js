7adc1be2da58c5d5502130ecf778f744a45aa1cc
Rizvi <rizvisharis@gmail.com>
2020-11-17 16:26:34 +0530

    getting weather location from geo api

---
+++ weather.js
@@ -0,0 +1,34 @@
+const API_KEY = "511b3031c8babf1e1978fc3bba435d42";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    //getWeather
+  }
+}
+function init() {
+  loadCoords();
+}
+
+init();
