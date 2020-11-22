476129da0895549951cdc6731ed5248ad086d19c
Rizvi <rizvisharis@gmail.com>
2020-11-22 16:37:31 +0530

    implement the api

--- weather.js
+++ weather.js
@@ -1,5 +1,20 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "511b3031c8babf1e1978fc3bba435d42";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSuccess(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 function handleGeoError() {
   console.log("Cant access geo location");
@@ -24,7 +40,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    //getWeather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 function init() {
